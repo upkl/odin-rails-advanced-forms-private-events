37cb8a22850e7d8fe959680d54162b1debe42396
upkl <2737384+upkl@users.noreply.github.com>
2023-08-26 21:21:14 +0200

    added event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,24 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = current_user.events.new
+  end
+
+  def create
+    @event = current_user.events.new(event_params)
+
+    if @event.save
+      redirect_to events_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :date, :location)
+  end
 end
