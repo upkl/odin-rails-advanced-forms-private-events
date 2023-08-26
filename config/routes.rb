e877ef8c329f978ec1c4ffc1914721a22c0c079a
upkl <2737384+upkl@users.noreply.github.com>
2023-08-26 20:14:25 +0200

    add user and event display

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: :show
+
+  resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
